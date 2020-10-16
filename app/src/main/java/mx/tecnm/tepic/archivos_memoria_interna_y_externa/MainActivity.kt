cc6b5037f29ac4b107fffb06d3ddc30410262416
kevinEsquivel <42071780+kevinEsquivel@users.noreply.github.com>
2020-10-16 10:51:51 -0600

    Initial commit

---
+++ app/src/main/java/mx/tecnm/tepic/archivos_memoria_interna_y_externa/MainActivity.kt
@@ -0,0 +1,15 @@
+package mx.tecnm.tepic.archivos_memoria_interna_y_externa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+}
